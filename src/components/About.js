f2bea22820d36abf84431a02648baecb71a5fc19
chriscrichter <chris_richter@outlook.com>
2020-04-16 17:15:59 -0400

    Updated js files.

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,10 @@ const About = () => {
       </div>
       <div className="right">
         <p>
-          Hello! My name is Chris. I enjoy programming and problem solving and I like the idea of creating amazing work that people use everyday. In addition to my education, I have worked in the computing field for many years. I have spent the last several years supporting various systems for a major media company as well as for prominent book, magazine and journal publishers. My responsibilities have included database development as well as general desktop and server support. As a recent graduate of the Columbia Engineering Coding Bootcamp, I am now pursuing a position that combines my database development experience and my knowledge of web technologies.
+          Hello! My name is Chris. I enjoy programming and problem solving and I like the idea of creating amazing work that people use everyday. In addition to my education, I have worked in the computing field for many years. I have spent the last several years supporting various systems for a major media company as well as for prominent book, magazine and journal publishers. My responsibilities have included database development as well as general desktop and server support.
+          </p>
+          <p>
+          As a recent graduate of the Columbia Engineering Coding Bootcamp, I am now pursuing a position that combines my database development experience and my knowledge of web technologies.
         </p>
       </div>
     </div>
