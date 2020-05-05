6a13ede7c4de58a4e8738d303237f0032a04a0f5
chriscrichter <chris_richter@outlook.com>
2020-05-05 10:05:43 -0400

    Updated js files.

--- src/components/Example.js
+++ src/components/Example.js
@@ -5,17 +5,13 @@ const Example = props => {
   return (
     <div className="example">
       <div className="title">
-      <a href={props.url} target="_blank">
         <h3>{props.title}</h3>
-         </a>
       </div>
       <div className = "info">
         <p>{props.text}</p>
       </div>
       <div className="image">
-      <a href={props.url} target="_blank">
         <img src={props.image} alt="" height="200px" width="300px" />
-        </a>
       </div>
       <div className="gitLink">
         <a href={props.url} target="_blank">{props.url}</a>
